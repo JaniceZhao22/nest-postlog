7bb904a3f3bd21ab19c0188afec230139fed7d51
zhaojiahui <zhaojiahui_22@163.com>
2022-01-18 17:29:48 +0800

    init

---
+++ src/posts/posts.entity.ts
@@ -0,0 +1,26 @@
+//    posts/posts.entity.ts
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("posts") // 创建一个posts的表
+export class PostsEntity {
+    @PrimaryGeneratedColumn()
+    id:number; // 标记为主列，值自动生成
+
+    @Column({ length:50 })
+    title: string;
+
+    @Column({ length: 20})
+    author: string;
+
+    @Column("text")
+    content:string;
+
+    @Column({default:''})
+    thumb_url: string;
+
+    @Column({type: 'timestamp', default: () => "CURRENT_TIMESTAMP"})
+    create_time: Date
+
+    @Column({type: 'timestamp', default: () => "CURRENT_TIMESTAMP"})
+    update_time: Date
+}
