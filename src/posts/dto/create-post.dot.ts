3509b49c7f675c5ada22fafb1b29c16541512b1b
zhaojiahui <zhaojiahui_22@163.com>
2022-01-20 16:14:57 +0800

    add： ValidationPipe 与 class-validator 进行校验

--- src/posts/dto/create-post.dot.ts
+++ src/posts/dto/create-post.dot.ts
@@ -1,10 +1,13 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreatePostDto {
   @ApiProperty({ description: '文章标题' })
+  @IsNotEmpty({ message: '文章标题必填' })
   readonly title: string;
 
   @ApiProperty({ description: '作者' })
+  @IsNotEmpty({ message: '缺少作者信息' })
   readonly author: string;
 
   @ApiPropertyOptional({ description: '内容' })
@@ -14,5 +17,6 @@ export class CreatePostDto {
   readonly cover_url: string;
 
   @ApiProperty({ description: '文章类型' })
+  @IsNumber()
   readonly type: number;
 }
